edda460c1fc3f2ebe436baa8653637d8bf2cc86c
bharathvaj68 <bharathvaj68@gmail.com>
2026-06-23 18:25:55 +0530

    deploy 3 try

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
               </>
             }
           />
-          <Route path="/product/:id" element={<ProductDetail />} />
+          {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </Router>
